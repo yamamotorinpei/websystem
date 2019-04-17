4295f6df56574af22681d54f626d88f019e0c03f
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2019-04-17 12:52:26 +0900

    ファイルを作成

---
+++ server2.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var server =http.createServer();
+
+server.on( 'request', function(req,res) {
+    res.writeHead( 200, {'Content-Type' : 'text/html' });
+    res.write('<!DOCTYPE html>');
+    res.write('<html lang=ja>');
+    res.write('<body>');
+    res.write('<h1>Hello world</h1>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
+
